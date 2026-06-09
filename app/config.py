ded6632dd6982aa02680f6e0a961f92bfdb5f92d
Shyam <karnam.r@peepal.co>
2026-06-09 06:04:19 +0530

    Add environment-driven config

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Config:
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    MONGO_DB = os.getenv("MONGO_DB", "imdb")
+    MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "movies")
+
+    IMPORT_BATCH_SIZE = int(os.getenv("IMPORT_BATCH_SIZE", "5000"))
+
+    DEFAULT_PAGE_SIZE = int(os.getenv("DEFAULT_PAGE_SIZE", "20"))
+    MAX_PAGE_SIZE = int(os.getenv("MAX_PAGE_SIZE", "100"))
